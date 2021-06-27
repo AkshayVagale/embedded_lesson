834b4bdeca4788f077e93563d982b1b3da790b10
annantagithub <39804118+annantagithub@users.noreply.github.com>
2021-06-27 23:42:20 +0530

    recursive

---
+++ 15-06-2021/decimal to binary conversion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int n, c, k;
+  printf("Enter decimal number \n");
+  scanf("%d", &n);
+  printf("%d It's binary number is:\n", n);
+  for (c = 5; c >= 0; c--)
+  {
+    k = n >> c;
+    if (k & 1)
+      printf("1");
+    else
+      printf("0");
+  }
+  return 0;
+}
+/*output
+enter decimal number5
+5 It's decimal number is; 000101
+*/
