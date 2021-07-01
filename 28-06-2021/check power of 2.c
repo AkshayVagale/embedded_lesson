a0fbede9c4b4bbc35732c5b844865d45be011a3e
annantagithub <39804118+annantagithub@users.noreply.github.com>
2021-07-02 04:28:37 +0530

    bit

---
+++ 28-06-2021/check power of 2.c
@@ -0,0 +1,17 @@
+/*Write a program to find the given number is 2 power or not.*/
+
+#include<stdio.h>
+#define power_of_2_or_not(num)           \
+({                                       \
+ if(((num) & (num - 1)) == 0)            \
+   printf("Power of 2\n");               \
+ else                                    \
+   printf("It is not power of 2\n");     \
+})
+int main()
+{
+    unsigned int num = 6;
+    power_of_2_or_not(num);
+}
+/*output
+It is not power of 2*/
